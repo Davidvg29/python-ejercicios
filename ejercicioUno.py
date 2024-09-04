a80ae80c2be8c1527bff510d9ea6e24ea6f23b65
David Valdez Gramajo <davidpivito@gmail.com>
2024-09-04 12:25:43 -0300

    adding files and ejercicie one

---
+++ ejercicioUno.py
@@ -0,0 +1,8 @@
+n1 = int(input("ingrese el primer numero entero "))
+n2 = int(input("ingrese el segundo numero entero "))
+n3 = int(input("ingrese el tercer numero entero "))
+
+suma = n1+n2+n3
+
+print("La sumatoria de los numerons es: ", suma)
+print("El promedio de los numeros es: ", suma/3)
