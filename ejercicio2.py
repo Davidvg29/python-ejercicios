176a0caa4270d0bfa54ceeedbbb41e4d1133eea9
David Valdez Gramajo <davidpivito@gmail.com>
2024-09-05 20:30:14 -0300

    adding exercise 2 and 3

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,11 @@
 # Dado el radio de un círculo, presentar por pantalla el diámetro, el perímetro y el
 # área del circulo
 
+radio = float(input("ingresa el radio del circulo :"))
+diametro = radio*2
+perimetro = 2 * 3.14 * radio
+area = 3.14 * radio ** 2
+
+print("El diametro del circulo es: ", diametro)
+print("El perimetro del circulo es: ", perimetro)
+print("El area del circulo es: ", area)
---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+# Ingresar tres números enteros, presentar por pantalla la suma de ellos.
+
+n1 = int(input("Ingrese el primer numero: "))
+n2 = int(input("Ingrese el segundo numero: "))
+n3 = int(input("Ingrese el tercer numero: "))
+
+print("La suma de los numeros es: ", n1+n2+n3)
