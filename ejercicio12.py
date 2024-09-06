e499fc9631b278b9620bc75720779445a9d0648d
David Valdez Gramajo <davidpivito@gmail.com>
2024-09-06 00:47:11 -0300

    add exercise 12

---
+++ ejercicio12.py
@@ -0,0 +1,8 @@
+# .Dada la base y la altura de un triángulo, presentar la superficie de este.
+
+base = float(input("Ingrese base: "))
+altura = float(input("Ingrese altura: "))
+
+superficie = (base*altura)/2
+
+print("La superficie del triangulo es de: ", superficie)
