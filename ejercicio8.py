a1e8b3d1dc650a7ad3c4f0ed8efb40dadea74fff
David Valdez Gramajo <davidpivito@gmail.com>
2024-09-06 00:46:32 -0300

    add exercise 8

---
+++ ejercicio8.py
@@ -0,0 +1,7 @@
+# Dado los lados de un rectángulo, presentar por pantalla su superficie 
+base = int(input("Ingrese la base de un rectangulo: "))
+altura = int(input("Ingrese la altura ded un rectangulo: "))
+
+superficie = base*altura
+
+print("La superficie del rectangulo es de: ", superficie, "cm")
