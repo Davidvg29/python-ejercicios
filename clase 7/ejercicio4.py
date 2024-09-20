629712363855fe5a1cbec16bb296bc44e1b26078
David Valdez Gramajo <davidpivito@gmail.com>
2024-09-20 19:47:31 -0300

    finished exercise 4 and not 5

--- clase 7/ejercicio4.py
+++ clase 7/ejercicio4.py
@@ -4,12 +4,12 @@
 pregunta = "si"
 contador = 0
 number = 0
-aux = 0
+aux = 1
 
 while pregunta=="si":
     number = int(input("ingresa un numero entero "))
-    if number < aux:
-        contador +=contador
+    if number <= aux:
+        contador = contador + 1
     aux = number
     pregunta = input("Desea ingresar otro numero? si o no: ")
 
---
+++ clase 7/ejercicio5.py
@@ -0,0 +1,2 @@
+# 5. Dado un numero natural de 4 o más dígitos, presentar por pantalla su invertido
+
