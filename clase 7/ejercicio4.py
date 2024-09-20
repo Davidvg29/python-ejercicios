d05f90cf0039bae80361b14d12f4c58900752aa9
David Valdez Gramajo <davidpivito@gmail.com>
2024-09-20 18:40:22 -0300

    add exercise 4

--- clase 7/ejercicio4.py
+++ clase 7/ejercicio4.py
@@ -2,7 +2,15 @@
 # números cargados.
 
 pregunta = "si"
+contador = 0
+number = 0
+aux = 0
 
 while pregunta=="si":
     number = int(input("ingresa un numero entero "))
-    
+    if number < aux:
+        contador +=contador
+    aux = number
+    pregunta = input("Desea ingresar otro numero? si o no: ")
+
+print("cantidad de series crecientes son: ", contador)
