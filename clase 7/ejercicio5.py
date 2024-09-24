27d783bd7c037d99271dffd19a6fe7d6e20709ba
David Valdez Gramajo <davidpivito@gmail.com>
2024-09-24 18:19:03 -0300

    add exercise 5

--- clase 7/ejercicio5.py
+++ clase 7/ejercicio5.py
@@ -1,2 +1,14 @@
 # 5. Dado un numero natural de 4 o más dígitos, presentar por pantalla su invertido
 
+number = int(input("Ingrese un numero para invertirlo "))
+invertido = 0
+
+while number<1000:
+    number = int(input("Ingrese un numero de 4 cifras"))
+
+while number > 0:
+    ultimo = number % 10
+    invertido = invertido * 10 + ultimo
+    number = number//10
+
+print("El numero invertido es ", invertido)
