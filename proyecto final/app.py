9801680406199388bfd747c4e6d975e190c84438
David Valdez Gramajo <davidpivito@gmail.com>
2024-11-15 17:35:08 -0300

    function mostrarTablaUltimosMovimientos listo

--- proyecto final/app.py
+++ proyecto final/app.py
@@ -2,11 +2,13 @@ from tkinter import *
 
 listaAlumnos=[
     [43501432, "David Valdez Gramajo", "vallistos 200v m6 c21", "Banda del Rio Sali", "Tucuman", 3813965671, "Masculino", "29/07/2001", "sin observacion", "david@gmail.com"],
-    [84757575, "Juan Mora", "vallistos 200v m6 c21", "Yerba Buena", "Tucuman", 3813123471, "Masculino", "29/07/2001", "sin observacion", "juan@gmail.com"]
+    [84757575, "Juan Mora", "vallistos 200v m6 c21", "Yerba Buena", "Tucuman", 3813123471, "Masculino", "29/07/2001", "sin observacion", "juan@gmail.com"],
+    [123, "Javier Milei", "vallistos 200v m6 c21", "Yerba Buena", "Tucuman", 3813123471, "Masculino", "29/07/2001", "sin observacion", "juan@gmail.com"]
 ]
 listaMovimientos = [
     [1, 43501432, "11/11/2024", "14:30", "17:00", "Micro cine"],
-    [2, 84757575, "15/11/2024", "15:30", "17:00", "Entretenimiento"]
+    [2, 84757575, "15/11/2024", "15:30", "17:00", "Entretenimiento"],
+    [3, 123, "35/11/2024", "27:30", "19:00", "capacitacion"]
 ]
 # ----------------INICIO FRONTEND----------------
 
@@ -36,34 +38,42 @@ def mostrarTablaUltimosMovimientos(listaMovimientos):
     areaUtilizadaLabel.grid(row=2, column=5, padx=0, pady=10, sticky="nsew")
     #fin titulo tablla de ultimos movimientos
 
+    #inicio contenido de tabla de ultimos movimientos
     contadorListaMovimientos = len(listaMovimientos)
     cont = 0
     row=3
     while cont < contadorListaMovimientos:
-        #inicio contenido de tabla de ultimos movimientos
-        dniEntry = Label(window, text="43501432", font=("Verdana",10), bg="red")
+        dniEntry = Label(window, text=listaMovimientos[cont][1], font=("Verdana",10), bg="red")
         dniEntry.grid(row=row, column=0, padx=0, pady=5, sticky="nsew")
 
-        nombreEntry = Label(window, text="David Valdez Gramajo", font=("Verdana",10))
+        nombre = ""
+        cont2 = 0
+        while len(listaAlumnos) > cont2:
+            lista = listaAlumnos[cont2]
+            if listaMovimientos[cont][1] in lista:
+                nombre = listaAlumnos[cont2][1]
+                break
+            cont2+=1
+        nombreEntry = Label(window, text=nombre, font=("Verdana",10))
         nombreEntry.grid(row=row, column=1, padx=0, pady=5, sticky="nsew")
 
-        fechaEntry = Label(window, text="11/11/2024", font=("Verdana",10))
+        fechaEntry = Label(window, text=listaMovimientos[cont][2], font=("Verdana",10))
         fechaEntry.grid(row=row, column=2, padx=0, pady=5, sticky="nsew")
 
-        horaEntadaEntry = Label(window, text="14:30", font=("Verdana",10))
+        horaEntadaEntry = Label(window, text=listaMovimientos[cont][3], font=("Verdana",10))
         horaEntadaEntry.grid(row=row, column=3, padx=0, pady=5, sticky="nsew")
 
-        horaSalidaEntry = Label(window, text="17:00", font=("Verdana",10))
+        horaSalidaEntry = Label(window, text=listaMovimientos[cont][4], font=("Verdana",10))
         horaSalidaEntry.grid(row=row, column=4, padx=0, pady=5, sticky="nsew")
 
-        areaUtilizadaEntry = Label(window, text="micro cine", font=("Verdana",10))
+        areaUtilizadaEntry = Label(window, text=listaMovimientos[cont][5], font=("Verdana",10))
         areaUtilizadaEntry.grid(row=row, column=5, padx=0, pady=5, sticky="nsew")
         #fin contenido tabla de ultimos movimientos
         
         row+=1
         cont+=1
 
-    #fin funcion mostrar tablas
+#fin funcion mostrar tablas
     
 # configuracion ventana principal
 window = Tk()
